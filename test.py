640206e8e058cb88cb6a0cb133444740d7c4cc0d
fairytail655 <2623538943@qq.com>
2020-08-17 15:12:38 +0800

    a

--- test.py
+++ test.py
@@ -8,11 +8,15 @@ import json
 from utils import *
 import time
 import pandas as pd
-
+from time import sleep
 from visualdl import LogWriter
+import os
+import shutil
 
 if __name__ == '__main__':
+    shutil.rmtree("./log/scalar_test/train")
     value = [i/1000.0 for i in range(1000)]
+    # for i in range(1000):
     # 初始化一个记录器
     with LogWriter(logdir="./log/scalar_test/train") as writer:
         for step in range(500):
@@ -20,4 +24,10 @@ if __name__ == '__main__':
             writer.add_scalar(tag="acc", step=step, value=value[step])
             # 向记录器添加一个tag为`loss`的数据
             writer.add_scalar(tag="loss", step=step, value=1/(value[step] + 1))
-            sleep(1)
+            sleep(0.1)
+        for step in range(500, 1000):
+            # 向记录器添加一个tag为`acc`的数据
+            writer.add_scalar(tag="acc", step=step, value=value[step])
+            # 向记录器添加一个tag为`loss`的数据
+            writer.add_scalar(tag="loss", step=step, value=1/(value[step] + 1))
+            sleep(0.1)
